0a60f8073606e5e2a71d9cd98d766b39c8b9f646
eschirtz <eschirtzinger@gmail.com>
2026-05-05 21:56:13 -0500

    fix hierarchy

--- vite.config.ts
+++ vite.config.ts
@@ -10,10 +10,6 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  build: {
-    outDir: '../dist',
-    emptyOutDir: true,
-  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
